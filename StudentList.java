c11ca487aeda02873251c95cf6136ab4f949fa10
roshitha2707 <72878563+roshita2707@users.noreply.github.com>
2022-01-05 11:36:49 +0530

    Add files via upload

---
+++ StudentList.java
@@ -0,0 +1,17 @@
+package jsonserdes;
+
+import java.util.ArrayList;
+
+public class StudentList {
+	
+	ArrayList<Student> studentlist;
+
+	public ArrayList<Student> getStudentlist() {
+		return studentlist;
+	}
+
+	public void setStudentlist(ArrayList<Student> studentlist) {
+		this.studentlist = studentlist;
+	}
+
+}
